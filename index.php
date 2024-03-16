7ca8c5707b7ab06355845e1c38ec7626fc3d0c53
mmartinde <manuel.delamontana@bootcamp-upgrade.com>
2024-03-16 14:05:51 +0100

    create a array of twitts

--- index.php
+++ index.php
@@ -4,4 +4,18 @@
     $autor = "Miguel de Cervantes";
     $usuario = "@miguelito";
 
+    
+    $twitts =[
+        ["texto"=>$texto,
+        "autor"=> $autor, 
+        "usuario"=>$usuario],
+        ["texto"=>"No hay vida sin peligro", 
+        "autor"=> "Perico de los Palotes", 
+        "usuario"=>"@Ppalotes"],
+        ["texto"=>"Yo soy tu padre",
+        "autor"=> "Dath Vader", 
+        "usuario"=>"@DarkV"],
+    ];
+
+    
     require_once "index.html";
